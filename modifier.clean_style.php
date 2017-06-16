57508fe3e6df3b06a4db5fcb0ad756003f17303a
AlekseyChan <aleksey.chan@gmail.com>
2017-06-16 16:09:51 +1100

    Update modifier.clean_style.php

--- modifier.clean_style.php
+++ modifier.clean_style.php
@@ -8,7 +8,7 @@
 
 function smarty_modifier_clean_style($text)
 {
-    return preg_replace('/(font-size:.*?;)|(width:.*?px;)|(height:.*?px;)|(font-family:.*?;)|(class=".*?")|(margin.*?;")|(background.*?;)|(line-height.*?;)|(text-indent.*?;)|(border.*?;)/imu', '', $text);
+    return preg_replace('/(font-size:.*?;)|(width:.*?px;)|(height:.*?px;)|(font-family:.*?;)|(class=".*?")|(margin.*?;)|(background.*?;)|(line-height.*?;)|(text-indent.*?;)|(border.*?;)/imu', '', $text);
 }
 
 ?>
